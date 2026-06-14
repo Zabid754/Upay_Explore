f0406b07401675bfb2ec87228ab3ad6e21c4266b
Zabid Rahman <i-zabid@upaybd.com>
2026-06-14 12:01:06 +0600

    fix: fix exchange in genericview and viewset

--- wallets/views.py
+++ wallets/views.py
@@ -13,6 +13,27 @@ from .serializers import WalletSerializer, TransactionSerializer, ExchangeReques
 # Create your views here.
 
 
+def perform_exchange(user, amount):
+        rate = Decimal('117.50')
+        try :
+            with transaction.atomic():
+                source = Wallet.objects.select_for_update().get(user=user, currency='USD')
+                if source.balance < amount:
+                    return {"error": "Insufficient USD","status": status.HTTP_400_BAD_REQUEST}
+                
+                dest, _ = Wallet.objects.get_or_create(user=user, currency='BDT')
+                source.balance -= amount
+                dest.balance += (amount * rate)
+                source.save()
+                dest.save()
+                
+                Transaction.objects.create(wallet=source, tx_type='EXCHANGE', amount=-amount, balance_after=source.balance)
+                Transaction.objects.create(wallet=dest, tx_type='EXCHANGE', amount=(amount*rate), balance_after=dest.balance)
+                return {"message": "Exchange Successful","status": status.HTTP_200_OK}
+        except Wallet.DoesNotExist:
+            return {"error": "You don't have a USD wallet.", "status": status.HTTP_404_NOT_FOUND}
+
+
 class TransactionHistoryAPIView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request):
@@ -35,42 +56,49 @@ class TransactionViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class ExchangeAPIView(APIView):
+    permission_classes=[IsAuthenticated]
     def post(self, request):
         serializer = ExchangeRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         amount = serializer.validated_data['amount']
-        result = self.perform_exchange(request.user, amount)
-        return Response(result, status=status.HTTP_200_OK)
+        result = perform_exchange(request.user, amount)
+        return Response(result, status=result.get('status'))
 
-    def perform_exchange(self, user, amount):
-        rate = Decimal('117.50')
-        with transaction.atomic():
-            source = Wallet.objects.select_for_update().get(user=user, currency='USD')
-            if source.balance < amount:
-                return {"error": "Insufficient USD"}
+    # def perform_exchange(self, user, amount):
+    #     rate = Decimal('117.50')
+    #     with transaction.atomic():
+    #         source = Wallet.objects.select_for_update().get(user=user, currency='USD')
+    #         if source.balance < amount:
+    #             return {"error": "Insufficient USD"}
             
-            dest, _ = Wallet.objects.get_or_create(user=user, currency='BDT')
-            source.balance -= amount
-            dest.balance += (amount * rate)
-            source.save()
-            dest.save()
+    #         dest, _ = Wallet.objects.get_or_create(user=user, currency='BDT')
+    #         source.balance -= amount
+    #         dest.balance += (amount * rate)
+    #         source.save()
+    #         dest.save()
             
-            Transaction.objects.create(wallet=source, tx_type='EXCHANGE', amount=-amount, balance_after=source.balance)
-            Transaction.objects.create(wallet=dest, tx_type='EXCHANGE', amount=(amount*rate), balance_after=dest.balance)
-            return {"message": "Exchange Successful (APIView)"}
+    #         Transaction.objects.create(wallet=source, tx_type='EXCHANGE', amount=-amount, balance_after=source.balance)
+    #         Transaction.objects.create(wallet=dest, tx_type='EXCHANGE', amount=(amount*rate), balance_after=dest.balance)
+    #         return {"message": "Exchange Successful (APIView)"}
+
+
 
 class ExchangeGenericView(generics.CreateAPIView):
     serializer_class = ExchangeRequestSerializer
+    permission_classes=[IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response({"message": "Exchange Successful (GenericView)"})
+        result = perform_exchange(request.user, serializer.validated_data['amount'])
+        return Response(result, status=result.get('status'))
 
 class ExchangeViewSet(viewsets.ViewSet):
+    permission_classes=[IsAuthenticated]
+
     @action(detail=False, methods=['post'])
     def do_exchange(self, request):
         serializer = ExchangeRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response({"message": "Exchange Successful (ViewSet Action)"})
-
+        result = perform_exchange(request.user, serializer.validated_data['amount'])
+        return Response(result, status=result.get('status'))
